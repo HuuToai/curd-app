8b770ee7bcdc02d6e8914c904f84d557f1752a4d
HuuToai <71dcht22129@student.utt.vn>
2024-03-22 00:59:25 +0700

    ver1.1  feature/addAccount

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -10,3 +10,14 @@
         {{ session('success') }}
     </div>
     @endif
+
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
